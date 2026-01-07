6a313f6b5551c074edeec3887def0cf17f29347c
bleach86 <14421739+bleach86@users.noreply.github.com>
2026-01-07 15:37:42 -0600

    url -> db_url

--- scripts/cloud_saves.sh
+++ scripts/cloud_saves.sh
@@ -39,7 +39,7 @@ SCRIPT_RAW_URL = f"{RAW_URL_BASE}/refs/heads/main/scripts/cloud_saves.sh"
 UPDATE_DB_RAW_URL = (
     f"{RAW_URL_BASE}_update_db/refs/heads/main/mister_cloud_saves_db.json"
 )
-LAUNCHER_SCRIPT_URL = "https://raw.githubusercontent.com/bleach86/mister_cloud_saves/a095cef07e6fd0f01c1874a56881e8d978f09438/scripts/cloud_saves_launcher.sh"  # f"{RAW_URL_BASE}/refs/heads/main/scripts/cloud_saves_launcher.sh"
+LAUNCHER_SCRIPT_URL = f"{RAW_URL_BASE}/refs/heads/main/scripts/cloud_saves_launcher.sh"
 MISTER_PATH = "/media/fat"
 CLIENT_DIR = os.path.join(MISTER_PATH, "cloud_saves")
 MISTER_LINUX_DIR = os.path.join(MISTER_PATH, "linux/")
@@ -375,7 +375,7 @@ def add_to_updater_if_needed():
         print("Adding Mister Cloud Saves database update to downloader.ini...")
 
         config["mister_cloud_saves"] = {}
-        config["mister_cloud_saves"]["url"] = UPDATE_DB_RAW_URL
+        config["mister_cloud_saves"]["db_url"] = UPDATE_DB_RAW_URL
 
         with open(
             os.path.join(MISTER_PATH, "downloader.ini"),
